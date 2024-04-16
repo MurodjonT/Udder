2d6edd693a1dcfa12f7398103ffbf25fab41af24
MurodjonT <murodjon.mmm.99@mail.ru>
2024-04-16 10:16:32 +0500

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Murodjon Turobov on 16/04/24.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
